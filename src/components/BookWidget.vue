c542228d9a89fdce09807391042b1b035d5a8223
Isabel <71656864+isabel-git@users.noreply.github.com>
2023-03-06 16:22:18 +0100

    displayed book widget

---
+++ src/components/BookWidget.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useStore } from '@/stores/store'
+
+const store = useStore()
+</script>
+
+<template>
+  <div class="box" v-for="book in store.getBooks">
+    <img :src="book.image" alt="book" />
+  </div>
+</template>
+
+<style scoped>
+.box {
+  width: 200px;
+  margin: auto;
+}
+img {
+  width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { useStore } from '@/stores/store'
+import { onMounted, defineProps } from 'vue'
+import BookWidget from '../components/BookWidget.vue'
+const store = useStore()
 
+onMounted(() => {
+  console.log('the component is now mounted')
+  store.loadBooks()
+})
 </script>
 
 <template>
-   <main>Home</main>
+  <main>
+    Home
+    <div v-if="store.getBooks" class="books">
+      <BookWidget v-for="_ in store.getBooks"></BookWidget>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.books {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
