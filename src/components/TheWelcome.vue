3112bf09d655703dbca0f2bec6305480258b378d
Psk-It <69162471+Psk-Ita@users.noreply.github.com>
2023-08-10 18:07:29 +0200

    v-model binding + suspense

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import Icon, { Icons } from './icons/Icon.vue'
+import { ref } from 'vue';
+import WelcomeItem from './WelcomeItem.vue';
+import Icon, { Icons } from './icons/Icon.vue';
+
+const vIcon0 = ref(Icons.Documentation);
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <Icon :icon="Icons.Documentation" :title="Icons.Documentation" />
+      <Icon v-model:icon="vIcon0" :title="vIcon0" />
     </template>
     <template #heading>Documentation</template>
 
@@ -17,17 +20,18 @@ import Icon, { Icons } from './icons/Icon.vue'
 
   <WelcomeItem>
     <template #icon>
-      <Icon :icon="Icons.Tooling" :title="Icons.Tooling" />
+      <Icon v-model:icon="Icons.Tooling" :title="Icons.Tooling" />
     </template>
     <template #heading>Tooling</template>
 
     This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The recommended IDE setup is
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If you need to test your components and web pages,
+    check out
+    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
+    and
     <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
 
     <br />
@@ -37,7 +41,7 @@ import Icon, { Icons } from './icons/Icon.vue'
 
   <WelcomeItem>
     <template #icon>
-      <Icon :icon="Icons.Ecosystem" :title="Icons.Ecosystem" />
+      <Icon v-model:icon="Icons.Ecosystem" :title="Icons.Ecosystem" />
     </template>
     <template #heading>Ecosystem</template>
 
@@ -45,38 +49,33 @@ import Icon, { Icons } from './icons/Icon.vue'
     <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
     <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
     <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If you need more resources, we suggest paying
     <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
     a visit.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <Icon :icon="Icons.Community" :title="Icons.Community" />
+      <Icon v-model:icon="Icons.Community" :title="Icons.Community" />
     </template>
     <template #heading>Community</template>
 
     Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official Discord server, or
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener">StackOverflow</a>. You should also subscribe to
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a>
+    and follow the official
     <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
     twitter account for latest news in the Vue world.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <Icon :icon="Icons.Support" :title="Icons.Support" />
+      <Icon v-model:icon="Icons.Support" :title="Icons.Support" />
     </template>
     <template #heading>Support Vue</template>
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
+    As an independent project, Vue relies on community backing for its sustainability. You can help us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
 </template>
--- src/components/icons/Icon.vue
+++ src/components/icons/Icon.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
-import type { PropType } from 'vue'
-import { defineAsyncComponent, watchEffect } from 'vue'
+import type { PropType } from 'vue';
+import { computed, defineAsyncComponent, markRaw } from 'vue';
 
 export enum Icons {
   Community = 'Community',
@@ -9,8 +9,8 @@ export enum Icons {
   Support = 'Support',
   Tooling = 'Tooling'
 }
-export type IconTypes = keyof typeof Icons
-export const IconOptions = Object.keys(Icons)
+export type IconTypes = keyof typeof Icons;
+export const IconOptions = Object.keys(Icons);
 
 export default {
   props: {
@@ -18,12 +18,30 @@ export default {
   },
   data() {
     return {
-      loadedIcon: defineAsyncComponent(() => import(`./Icon${this.$props.icon}.vue`))
-    }
-  }
-}
+      sourceIcon: this.$props.icon,
+      currentIcon: markRaw(defineAsyncComponent(() => import(`./Icon${this.sourceIcon}.vue`))),
+      nextIcon: computed(() => {
+        const nextIndex = (1 + IconOptions.findIndex((x) => x === this.$props.icon)) % IconOptions.length;
+        return IconOptions[nextIndex];
+      })
+    };
+  },
+  updated() {
+    // prevent: Maximum recursive updates exceeded
+    if (this.sourceIcon === this.$props.icon) return;
+    this.sourceIcon = this.$props.icon;
+    this.currentIcon = markRaw(defineAsyncComponent(() => import(`./Icon${this.sourceIcon}.vue`)));
+  },
+  inheritAttrs: false
+};
 </script>
 
 <template>
-  <component :is="loadedIcon" :key="$props.icon" :alt="$props.icon" />
+  <Suspense>
+    <!-- main content -->
+    <component v-bind="$attrs" :is="currentIcon" :id="$props.icon" :key="$props.icon" :alt="$props.icon" @click="$emit('update:icon', nextIcon)" />
+
+    <!-- loading state -->
+    <template #fallback><span>...</span></template>
+  </Suspense>
 </template>
