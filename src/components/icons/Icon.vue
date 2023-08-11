c346c2e9a77b0d9f626d63c35bab783a14641170
Psk-It <69162471+Psk-Ita@users.noreply.github.com>
2023-08-11 17:23:22 +0200

    explicitly setup composition api

--- src/components/icons/Icon.vue
+++ src/components/icons/Icon.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import type { PropType } from 'vue';
-import { computed, defineAsyncComponent, markRaw } from 'vue';
+import { ref, computed, watchEffect, markRaw } from 'vue';
 
 export enum Icons {
   Community = 'Community',
@@ -16,32 +16,55 @@ export default {
   props: {
     icon: String as PropType<IconTypes>
   },
-  data() {
+  setup(props, { attrs, emit }) {
+    const loading = ref(true);
+    const sourceIcon = ref(props.icon);
+    const currentIcon = ref(null);
+
+    const nextIcon = computed(() => {
+      const nextIndex = (1 + IconOptions.findIndex((x) => x === props.icon)) % IconOptions.length;
+      return IconOptions[nextIndex] as IconTypes;
+    });
+
+    const loadIcon = async (iconType?: IconTypes): Promise<void> => {
+
+      if (!iconType) return Promise.resolve();
+
+      loading.value = true;
+
+      await new Promise((resolve) => setTimeout(resolve, 199));
+
+      const objIcon = await import(`./Icon${iconType}.vue`);
+      currentIcon.value = markRaw(objIcon.default);
+
+      loading.value = false;
+
+      return Promise.resolve();
+    }
+
+    watchEffect(async () => {
+      await loadIcon(props.icon)
+    });
+
     return {
-      sourceIcon: this.$props.icon,
-      currentIcon: markRaw(defineAsyncComponent(() => import(`./Icon${this.sourceIcon}.vue`))),
-      nextIcon: computed(() => {
-        const nextIndex = (1 + IconOptions.findIndex((x) => x === this.$props.icon)) % IconOptions.length;
-        return IconOptions[nextIndex];
-      })
-    };
-  },
-  updated() {
-    // prevent: Maximum recursive updates exceeded
-    if (this.sourceIcon === this.$props.icon) return;
-    this.sourceIcon = this.$props.icon;
-    this.currentIcon = markRaw(defineAsyncComponent(() => import(`./Icon${this.sourceIcon}.vue`)));
+      attrs,
+      loading,
+      nextIcon,
+      sourceIcon,
+      currentIcon,
+      emit,
+      loadIcon,
+    }
   },
   inheritAttrs: false
 };
 </script>
 
 <template>
-  <Suspense>
-    <!-- main content -->
-    <component v-bind="$attrs" :is="currentIcon" :id="$props.icon" :key="$props.icon" :alt="$props.icon" @click="$emit('update:icon', nextIcon)" />
-
-    <!-- loading state -->
-    <template #fallback><span>...</span></template>
-  </Suspense>
+  <!-- loading state -->
+  <template v-if="loading">
+    <span>...</span>
+  </template>
+  <component v-else-if="currentIcon" v-bind="attrs" :is="currentIcon" :id="icon" :key="icon" :alt="icon"
+    @click="emit('update:icon', nextIcon)" />
 </template>
